777c11cc13f8c69e5a5b34d06b2a2f09a62df4e8
Misahael Fernandez <2372351f@student.gla.ac.uk>
2019-01-20 17:29:20 +0000

    End Chapter 4 Static files, media files and templates

--- rango/views.py
+++ rango/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    response = 'Rango says hey there partner! ' + '<a href="/rango/about/">About</a>'
-    return HttpResponse(response)
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
-    response = 'Rango says here is the about page. ' + '<a href="/rango/">Index</a>'
-    return HttpResponse(response)
+    context_dict = {'username': "Misahael"}
+    return render(request, 'rango/about.html', context=context_dict)
