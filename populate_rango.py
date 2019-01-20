77fad7a441015d7e4d498448e494d474128331fd
Misahael Fernandez <2372351f@student.gla.ac.uk>
2019-01-20 22:16:41 +0000

    Test execution

--- populate_rango.py
+++ populate_rango.py
@@ -1,13 +1,10 @@
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-'tango_with_django_project.settings')
+                      'tango_with_django_project.settings')
 
 import django
 django.setup()
 from rango.models import Category, Page
-
-
-django.setup()
 
 
 def populate():
@@ -39,9 +36,9 @@ def populate():
         {"title": "Flask",
          "url": "http://flask.pocoo.org"}]
 
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworks": {"pages": other_pages}}
+    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+            "Django": {"pages": django_pages, "views": 64, "likes": 32},
+            "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16}}
 
     # If you want to add more catergories or pages,
     # add them to the dictionaries above.
@@ -53,7 +50,7 @@ def populate():
     # for more information about how to iterate over a dictionary properly.
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
 
@@ -71,8 +68,8 @@ def add_page(cat, title, url, views=0):
     return p
 
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name, views, likes):
+    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
     c.save()
     return c
 
